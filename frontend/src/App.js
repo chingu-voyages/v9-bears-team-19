9e609d349d9bffa50389f478508b9396ba039d8a
Matt Player <mattplayer@mac.com>
2019-06-07 22:34:01 +0100

    create basic signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import { Container } from "semantic-ui-react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { gql } from "apollo-boost";
 import Users from "./components/Users";
 
 import TopNavbar from "./components/TopNavbar";
 import Placeholder from "./components/Placeholder";
+import Signup from "./components/Signup";
 
 const client = new ApolloClient({
 	uri: process.env.REACT_APP_APOLLO_SERVER_URL
@@ -24,6 +24,7 @@ function App() {
 					<Placeholder />
 				</Container>
 				<Users />
+				<Signup />
 			</div>
 		</ApolloProvider>
 	);
