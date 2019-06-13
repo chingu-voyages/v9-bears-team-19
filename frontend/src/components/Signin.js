6ac71cf2733e15e32c4d9233133d7a0f5c44290e
Matt Player <mattplayer@mac.com>
2019-06-13 14:23:25 +0100

    Created Signin Component

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
 
-const Signin = props => {
-	// todo create login component
-	return <p>Login Component</p>;
+const Signin = () => {
+	const [formValues, setFormValues] = useState({});
+
+	const handleChange = e => {
+		e.preventDefault();
+		setFormValues({ ...formValues, [e.target.name]: e.target.value });
+	};
+
+	const SIGNIN_MUTATION = gql`
+		mutation SIGNUP_MUTATION($email: String!, $password: String!) {
+			loginUser(email: $email, password: $password) {
+				id
+			}
+		}
+	`;
+
+	return (
+		<Mutation mutation={SIGNIN_MUTATION} variables={formValues}>
+			{loginUser => {
+				return (
+					<form
+						method="post"
+						onSubmit={e => {
+							e.preventDefault();
+							loginUser();
+						}}
+					>
+						<fieldset>
+							<h2>Sign In</h2>
+							<label htmlFor="email">
+								Email:
+								<input name="email" onChange={handleChange} />
+							</label>
+							<label>
+								Password:
+								<input
+									name="password"
+									type="password"
+									onChange={handleChange}
+								/>
+							</label>
+							<button type="submit">Submit</button>
+						</fieldset>
+					</form>
+				);
+			}}
+		</Mutation>
+	);
 };
 
 export default Signin;
---
+++ frontend/src/components/Signout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signout = props => {
+	return <p>Signout Component</p>;
+};
+
+export default Signout;
